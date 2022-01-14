309a17b5a955fbb0970767ef4deb6dd99c1e6a24
Uchchash346 <mru416@gmail.com>
2022-01-14 21:26:09 +0600

    final touch

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Navbar.css'
 import menuImage from '../../images/menu.png'
 import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 const Navbar = () => {
     return (
         <>
@@ -9,7 +10,7 @@ const Navbar = () => {
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">Tech Coach</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"><FaBars size={25} color='black' /></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
